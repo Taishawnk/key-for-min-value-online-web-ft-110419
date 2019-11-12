8e9f64a19b11cad61f6e8c19bdb57b7db0f1152c
Taishawn King <taishawnking1@yahoo.com>
2019-11-12 22:19:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-ma_val = 100 
-m_k
+mi_val = 100 
+mi_k = nil 
+name_hash.each do |k,v|
+  if v < mi_val 
+    mi_val = v
 
 end
