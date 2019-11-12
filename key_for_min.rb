869684c9ca729f17cbe5ae5b67e5f6498bb43c4c
Taishawn King <taishawnking1@yahoo.com>
2019-11-12 22:23:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+5 6 95
 def key_for_min_value(name_hash)
 mi_val = 100 
 mi_k = nil 
 name_hash.each do |k,v|
   if v < mi_val 
     mi_val = v
-  mi_k = k
+    mi_k = k
 
 end
